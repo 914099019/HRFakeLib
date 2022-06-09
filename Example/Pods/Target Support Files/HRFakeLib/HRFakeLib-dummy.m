2cdb426a2c92b2a2b97e2ee03a9842ca992475cd
lihaoran22 <lihaoran22@meituan.com>
2022-06-09 14:11:10 +0800

    chore: Update .gitignore

--- Example/Pods/Target Support Files/HRFakeLib/HRFakeLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HRFakeLib : NSObject
-@end
-@implementation PodsDummy_HRFakeLib
-@end
--- Example/Pods/Target Support Files/Pods-HRFakeLib_Example/Pods-HRFakeLib_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HRFakeLib_Example : NSObject
-@end
-@implementation PodsDummy_Pods_HRFakeLib_Example
-@end
